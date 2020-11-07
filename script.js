36624add44b0db4cc93b172fd4f7e07e2483799f
Sanya Gupta <sanyagupta319@gmail.com>
2020-11-07 23:38:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,95 @@
+var questionCount=0;
+var lowCarbonFootprintScore=0;
+var middleCarbonFootprintScore=0;
+var highCarbonFootprintScore=0;
+
+var q1a1=document.getElementById("q1a1");
+var q1a2=document.getElementById("q1a2");
+var q1a3=document.getElementById("q1a3");
+
+var q2a1=document.getElementById("q2a1");
+var q2a2=document.getElementByID("q2a2");
+var q2a3=document.getElementById("q2a3");
+
+var q3a1=document.getElementById("q3a1");
+var q3a2=document.getElementById("q3a2");
+var q3a3=document.getElementById("q3a3");
+
+var q4a1=document.getElementById("q4a1");
+var q4a2=document.getElementById("q4a2");
+var q4a3=document.getElementById("q4a3");
+
+var q5a1=document.getElementById("q5a1");
+var q5a2=document.getElementById("q5a2");
+var q5a3=document.getElementById("q5a3");
+
+var q6a1=document.getElementById("q6a1");
+var q6a2=document.getElementById("q6a2");
+var q6a3=document.getElementById("q6a3");
+
+var q7a1=document.getElementById("q7a1");
+var q7a2=document.getElementById("q7a2");
+var q7a3=document.getElementById("q7a3");
+
+q1a3.addEventListener("click", highCarbonFootprint);
+q1a2.addEventListener("click", middleCarbonFootprint);
+q1a1.addEventListener("click", lowCarbonFootprint);
+
+q2a1.addEventListener("click", lowCarbonFootprint);
+q2a2.addEventListener("click", middleCarbonFootprint);
+q2a3.addEventListener("click", highCarbonFootprint);
+
+q3a1.addEventListener("click", lowCarbonFootprint);
+q3a2.addEventListener("click", middleCarbonFootprint);
+q3a3.addEventListener("click", highCarbonFootprint);
+
+q4a1.addEventListener("click", lowCarbonFootprint);
+q4a2.addEventListener("click", middleCarbonFootprint);
+q4a3.addEventListener("click", highCarbonFootprint);
+
+q5a1.addEventListener("click", lowCarbonFootprint);
+q5a2.addEventListener("click", middleCarbonFootprint);
+q5a3.addEventListener("click", highCarbonFootprint);
+
+q6a1.addEventListener("click", lowCarbonFootprint);
+q6a2.addEventListener("click", middleCarbonFootprint);
+q6a3.addEventListener("click", highCarbonFootprint);
+
+q7a1.addEventListener("click", lowCarbonFootprint);
+q7a2.addEventListener("click", middleCarbonFootprint);
+q7a3.addEventListener("click", highCarbonFootprint);
+
+function lowCarbonFootprint() {
+  console.log("running")
+  lowCarbonFootprintScore += 1;
+  questionCount += 1;
+  if (questionCount >= 7){
+    updateResult();
+  }
+}
+function middleCarbonFootprint() {
+  middleCarbonFootprintScore += 1;
+  questionCount +=1;
+  if (questionCount >= 7){
+    updateResult();
+  }
+}
+function highCarbonFootprint() {
+  highCarbonFootprintScore += 1;
+  questionCount +=1;
+  if (questionCount >= 7){
+    updateResult();
+  }
+}
+
+function updateResult() {
+  if (lowCarbonFootprintScore >= 4) {
+    result.innerHTML = "You have a low carbon footprint!";
+  } else if (middleCarbonFootprintScore >= 4) {
+    result.innerHTML = "You have an average carbon footprint!";
+  } else if(highCarbonFootprintScore >= 4) {
+    result.innerHTML = "You have a high carbon footprint!";
+  } else {
+    result.innerHTML = "Your carbon footprint could not be determined. Try again!";
+  }
+}
---
+++ script1.js
@@ -0,0 +1,134 @@
+(function(){
+  function buildQuiz(){
+    const output = [];
+
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+
+        const answers = [];
+         for(letter in currentQuestion.answers){
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+        output.push(
+          `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+        );
+      }
+    );
+
+    quizContainer.innerHTML = output.join('');
+  }
+
+  function showResults(){
+
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    let numCorrect = 0;
+
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      if(userAnswer === currentQuestion.correctAnswer){
+        numCorrect++;
+
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      else{
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const myQuestions = [
+    {
+      question: "How many members live in your household?",
+      answers: {
+        a: "None",
+        b: "1",
+        c: "2",
+        d: "3",
+        e: "4",
+        f: "5",
+        g: "More than 5"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "How big is your house?",
+      answers: {
+        a: "Large",
+        b: "Medium-sized",
+        c: "Small",
+        d: "Apartment"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Evaluate your food choices.",
+      answers: {
+        a: "Do you eat domestic meat on a daily basis?",
+        b: "Do you eat domestic meat a few times per week?",
+        c: "Are you a vegan or only eat wild meat"
+      },
+      correctAnswer: "a"
+    }
+  ];
+
+  buildQuiz();
+
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  showSlide(currentSlide);
+
+  submitButton.addEventListener('click', showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+})();
