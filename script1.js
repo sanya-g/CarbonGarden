c0652f20d537062bc9435e2e043db8f766787fc2
Sanya Gupta <sanyagupta319@gmail.com>
2020-11-08 05:08:51 +0000

    debugging

--- script1.js
+++ script1.js
@@ -1,4 +1,4 @@
-  function updateResult {
+
   if (middleCarbonFootprintScore >= 4) {
     document.getElementById("plant4").style.display = "hidden";
     document.getElementById("plant6").style.display = "hidden";
@@ -8,5 +8,4 @@
     document.getElementById("plant5").style.display = 'none';
     document.getElementById("plant6").style.display = 'none';
   } else {
-  }
   }
