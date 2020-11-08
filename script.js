d5f9690baf2771c9bd79fb08ec924eb7cbb7b1e8
Sanya Gupta <sanyagupta319@gmail.com>
2020-11-08 04:47:30 +0000

    trying to debug

--- script.js
+++ script.js
@@ -101,3 +101,8 @@ function updateResult() {
     result.innerHTML = "Your carbon footprint could not be determined. Try again!";
   }
 }
+// function plantImages() {
+ // if (middleCarbonFootprintScore >= 4) {
+  //  result.innerHTML=document.getElementById("plant4").style.visibility= hidden;
+  //}
+//}
---
+++ script1.js
@@ -0,0 +1,10 @@
+  if (middleCarbonFootprintScore >= 4) {
+    document.getElementById("plant4").style.visibility = hidden;
+    document.getElementById("plant6").style.visibility = hidden;
+  } else if(highCarbonFootprintScore >= 4) {
+    document.getElementById("plant3").style.visibility = hidden;
+    document.getElementById("plant4").style.visibility = hidden;
+    document.getElementById("plant5").style.visibility = hidden;
+    document.getElementById("plant6").style.visiblity = hidden;
+  } else {
+  }
