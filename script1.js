291861640c44e658b2ea8c250f0f0bb7760aac14
Sanya Gupta <67162917+sanya-g@users.noreply.github.com>
2020-11-07 22:02:02 -0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -2,18 +2,20 @@
 //var mIDDLELS = localStorage.getItem("mIDDLELocalStorage");
 //var hIGHLS = localStorage.getItem("hIGHLocalStorage");
 
-var number = 123;  
-localStorage.setItem("lOWLS", number);  
-var value = localStorage.getItem("lOWLS"); 
-var middleCarbonFootprint = Number(value);  
+//var number = 123;  
+//localStorage.setItem("lOWLS", number);  
+//var value = localStorage.getItem("lOWLS"); 
+//var middleCarbonFootprint = Number(value);  
 
-middleCarbonFootprint = Number(lOWLS);
-highCarbonFootprint = Number(hIGHLS);
+//middleCarbonFootprint = Number(lOWLS);
+//highCarbonFootprint = Number(hIGHLS);
 
-if (middleCarbonFootprint >= 4) {
+import(middleCarbonFootprintScore, highCarbonFootprintScore);
+
+if (middleCarbonFootprintScore >= 4) {
     document.getElementById("plant4").style.display = "hidden";
     document.getElementById("plant6").style.display = "hidden";
-  } else if(highCarbonFootprint >= 4) {
+  } else if(highCarbonFootprintScore >= 4) {
     document.getElementById("plant3").style.display = 'none';
     document.getElementById("plant4").style.display = 'none';
     document.getElementById("plant5").style.display = 'none';
