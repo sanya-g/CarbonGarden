235c59af8cc8b531dbe12985eea654bb8eeb6031
Sanya Gupta <sanyagupta319@gmail.com>
2020-11-08 02:55:13 +0000

    edited garden.html to make images clickable, adding # plant functionality

--- original.js
+++ original.js
@@ -87,35 +87,11 @@ function highCarbonFootprint() {
 function updateResult() {
   if (lowCarbonFootprintScore >= 4) {
     result.innerHTML = "You have a low carbon footprint!";
-    "plant5".a {
-      visibility: hidden;
-    }   
   } else if (middleCarbonFootprintScore >= 4) {
     result.innerHTML = "You have an average carbon footprint!";
   } else if(highCarbonFootprintScore >= 4) {
     result.innerHTML = "You have a high carbon footprint!";
-    "plant4".a {
-      visibility: hidden;
-    } 
-    "plant5".a {
-      visibility: hidden;
-    } 
-    "plant6".a {
-      visibility: hidden;
-    } 
   } else {
     result.innerHTML = "Your carbon footprint could not be determined. Try again!";
-    "plant3".a {
-      visibility: hidden;
-    } 
-    "plant4".a {
-      visibility: hidden;
-    } 
-    "plant5".a {
-      visibility: hidden;
-    } 
-    "plant6".a {
-      visibility: hidden;
-    } 
   }
 }
